4403788de4a2bd223f7e685940397aebce725d55
Beto Costa <jjbetocosta23@gmail.com>
2023-02-18 23:19:26 -0300

    var

--- js/var.js
+++ js/var.js
@@ -6,7 +6,7 @@ var comprimento = 7;
 area = altura * comprimento;
 console.log(area)
 var area;
-*/
+
 // let
 
 let forma = "retângulo";
@@ -15,6 +15,20 @@ let comprimento = 7;
 let area;
 
 if (forma === "retângulo") {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
+*/
+
+const forma = 'triângulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
     area = altura * comprimento;
 } else {
     area = (altura * comprimento) / 2;
